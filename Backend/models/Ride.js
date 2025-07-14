8e5c44b1a8ccee861c7dda7b18000c19b61cafa9
SuryaJanardhanGS <surya@growstack.biz>
2025-07-14 18:20:15 +0530

    ride schema added

--- Backend/models/Ride.js
+++ Backend/models/Ride.js
@@ -1,1 +1,30 @@
 const mongoose = require('mongoose');
+
+const rideSchema = new mongoose.Schema({
+    driver: mongoose.Schema.Types.ObjectId,
+    user: mongoose.Schema.Types.ObjectId,
+    startLocation: {
+        type: String,
+        required: true
+    },
+    endLocation: {
+        type: String,
+        required: true
+    },
+    distance: {
+        type: Number,
+        required: true
+    },
+    fare: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    createdAt: Date,
+    updatedAt: Date
+});
+
+module.exports = mongoose.model('Ride', rideSchema);
