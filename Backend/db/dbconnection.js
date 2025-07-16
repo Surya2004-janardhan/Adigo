405b6e8b31111370be185a1c758a19e614fbb25c
SuryaJanardhanGS <surya@growstack.biz>
2025-07-16 23:01:26 +0530

    feat : mongo connected && server running fine && all routes set-up

--- Backend/db/dbconnection.js
+++ Backend/db/dbconnection.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
+const MONGO_URI =
+  "mongodb+srv://chintalajanardhan2004:123@cluster0.dm083rc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connecttoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-    });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(MONGO_URI, {});
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-
+    console.error("Error connecting to MongoDB:", error);
   }
-}
+};
 
 module.exports = connecttoDB;
--- Backend/routes/ridesRoute.js
+++ Backend/routes/ridesRoute.js
@@ -22,4 +22,39 @@ router.get("/rides", middleware, async (req, res) => {
   }
 });
 
+router.patch("/complete/:ride_id", middleware, async (req, res) => {
+  try {
+    const ride_id = req.params.ride_id;
+    const ride = await Ride.findOne({ id: ride_id });
+    if (ride) {
+      ride.status = "completed";
+      await ride.save();
+      res.status(200).json({ message: "Ride completed successfully" });
+    } else {
+      res.status(404).json({ message: "Ride not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+
+router.patch("/cancle/:ride_id", middleware, async (req, res) => {
+  try {
+    const ride_id = req.params.ride_id;
+    const ride = await Ride.findOne({ id: ride_id });
+    if (ride) {
+      ride.status = "cancled";
+      await ride.save();
+      res.status(200).json({ message: "Ride cancled successfully" });
+    } else {
+      res.status(404).json({ message: "Ride not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
 module.exports = router;
