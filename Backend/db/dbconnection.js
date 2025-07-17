9d03dec0a21eb1223284045d3210c6563c93eb8d
SuryaJanardhanGS <surya@growstack.biz>
2025-07-17 09:51:46 +0530

    uri to .env

--- Backend/db/dbconnection.js
+++ Backend/db/dbconnection.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-const MONGO_URI =
-  "mongodb+srv://chintalajanardhan2004:123@cluster0.dm083rc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+// const dotenv = require("dotenv");
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+// console.log(MONGO_URI)
 const connecttoDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {});
+    await mongoose.connect( MONGO_URI, {});
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
