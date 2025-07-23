8b04ee79e8f0b3af9d5780a6648ec6f5dc665e48
SuryaJanardhanGS <surya@growstack.biz>
2025-07-23 11:22:43 +0530

    feat : gud mrng

--- Frontend/notifications/PushNotifications.js
+++ Frontend/notifications/PushNotifications.js
@@ -1,7 +1,7 @@
 import * as Notifications from "expo-notifications";
 import * as Device from "expo-device";
 import { Platform } from "react-native";
-
+// hoping this shit works
 // Configure notification handler (foreground behavior)
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
