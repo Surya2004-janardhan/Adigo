0291e218dcb066945315ee8fdb45ede2c7db62da
SuryaJanardhanGS <surya@growstack.biz>
2025-07-24 17:04:53 +0530

    refactor : auto login jwt login setup

--- Backend/db/dbconnection.js
+++ Backend/db/dbconnection.js
@@ -5,7 +5,7 @@ const MONGO_URI = process.env.MONGO_URI;
 // console.log(MONGO_URI)
 const connecttoDB = async () => {
   try {
-    await mongoose.connect( MONGO_URI, {});
+    await mongoose.connect(MONGO_URI, {});
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
-  console.log("route called")
+  console.log("route called");
   try {
     const { name, rollno, email, phonenumber, password } = req.body;
-    console.log(req.body)
+    console.log(req.body);
     if (name && rollno && email && phonenumber && password) {
       const user = await User.create({
         name,
@@ -17,37 +17,46 @@ router.post("/register", async (req, res) => {
         phonenumber,
         password,
       });
-      console.log(process.env.JWT_SECRET)
+      console.log(process.env.JWT_SECRET);
       const token = await jwt.sign(
         { user: user.rollno },
         process.env.JWT_SECRET,
         { expiresIn: "30d" }
       );
-      res
-        .status(201)
-        .json({ message: "User created successfully", user: user, token:token });
-        // console.log(token)
+      res.status(201).json({
+        message: "User created successfully",
+        user: user,
+        token: token,
+      });
+      // console.log(token)
       // await localStorage.setItem("token", token);
       // res
-        // .status(201)
-        // .json({ message: "User created successfully", user: user });
+      // .status(201)
+      // .json({ message: "User created successfully", user: user });
     } else {
       res.status(400).json({ message: "Please provide all details" });
     }
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
     res.status(400).json({ message: "Incorrect details" });
   }
 });
 
 router.post("/login", async (req, res) => {
   try {
+    console.log("inside of login");
     const { rollno, password } = req.body;
     if (rollno && password) {
       const user = await User.findOne({ rollno: rollno });
       // for now direct checking + later add bcrypt hash to safe store passwords
       if (user && user.password === password) {
-        res.status(200).json({ message: "Login successful", user: user });
+        console.log("succesfful login");
+        const token = await jwt.sign(
+          { user: user.rollno },
+          process.env.JWT_SECRET,
+          { expiresIn: "30d" }
+        );
+        res.status(200).json({ message: "Login successful", user: user ,token : token});
       } else {
         res.status(401).json({ message: "Invalid credentials" });
       }
@@ -55,6 +64,7 @@ router.post("/login", async (req, res) => {
       res.status(400).json({ message: "Please provide rollno and password" });
     }
   } catch (E) {
+    console.log(E.message);
     res
       .status(500)
       .json({ message: "Internal server error", error: E.message });
